df85bff4d4ec8b8825e89925319adf5ee090ab7b
kovacsda <lion.myip@gmail.com>
2014-06-15 22:21:50 +0200

    Use own bytearray data type and gray scale image

--- ui-java/modules/imaging/src/main/java/kr/promu/caireen/imaging/workflow/counter/CounterWorkFlow.java
+++ ui-java/modules/imaging/src/main/java/kr/promu/caireen/imaging/workflow/counter/CounterWorkFlow.java
@@ -1,16 +1,18 @@
 package kr.promu.caireen.imaging.workflow.counter;
 
+import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorConvertOp;
 import java.awt.image.DataBufferByte;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.swing.JOptionPane;
 
 import kr.promu.caireen.common.interceptor.ImageInterceptor;
 import kr.promu.caireen.common.service.ServiceObserver;
 import kr.promu.caireen.common.workflow.DefaultWorkflow;
 import kr.promu.caireen.imaging.workflow.CounterProcessEvent;
+import kr.promu.caireen.jni.data.ByteArray;
 import kr.promu.caireen.jni.service.NativeImageProcessor;
 
 public class CounterWorkFlow extends DefaultWorkflow<CounterProcessEvent> {
@@ -35,21 +37,31 @@ public class CounterWorkFlow extends DefaultWorkflow<CounterProcessEvent> {
     }
 
     private int getDropCount(final BufferedImage image) {
-	JOptionPane.showMessageDialog(null, "Tibi vazzeg!!!");
-	byte[] imageData = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
-	DataBufferByte requestData = new DataBufferByte(imageData.length + 4 * 5);
-	requestData.setElem(0, 1);
-	requestData.setElem(4, image.getWidth());
-	requestData.setElem(8, image.getHeight());
-	requestData.setElem(12, image.getWidth());
-	requestData.setElem(16, 0); // format grayscale8
-	for (int i = 0; i < imageData.length; i++) {
-	    requestData.getData()[i + 4 * 5] = imageData[i];
-	}
-	byte[] result = nativeImageProcessor.request(requestData.getData());
+	byte[] imageData = getGrayScaleImage(image);
+	ByteArray requestData = generateRequest(image, imageData);
+	byte[] result = nativeImageProcessor.request(requestData.getDataArray());
 	DataBufferByte resultData = new DataBufferByte(result, result.length);
 	System.out.println(resultData.getElem(0));
 	return 0;
+    }
+
+    private ByteArray generateRequest(final BufferedImage image, final byte[] imageData) {
+	ByteArray requestData = new ByteArray(imageData.length + 20);
+	requestData.setData(0, 1);
+	requestData.setData(4, image.getWidth());
+	requestData.setData(8, image.getHeight());
+	requestData.setData(12, image.getWidth());
+	requestData.setData(16, 0); // format gray scale
+	requestData.setData(20, imageData);
+	return requestData;
+    }
+
+    private byte[] getGrayScaleImage(final BufferedImage image) {
+	ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
+	ColorConvertOp op = new ColorConvertOp(cs, null);
+	BufferedImage grayScaleImage = op.filter(image, null);
+	byte[] imageData = ((DataBufferByte) grayScaleImage.getRaster().getDataBuffer()).getData();
+	return imageData;
     }
 
     private void fireEvent(final int dropPosition) {
