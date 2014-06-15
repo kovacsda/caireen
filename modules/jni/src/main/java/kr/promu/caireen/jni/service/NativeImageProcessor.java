14500bacb9cb05eb501e256efd91655d846517a8
kovacsda <lion.myip@gmail.com>
2014-06-15 15:48:19 +0200

    Maven nativ plugin

--- modules/jni/src/main/java/kr/promu/caireen/jni/service/NativeImageProcessor.java
+++ modules/jni/src/main/java/kr/promu/caireen/jni/service/NativeImageProcessor.java
@@ -6,6 +6,30 @@ public class NativeImageProcessor {
 	System.load("C:/_dev/digitron/caireen/working_copy/modules/jni/src/main/resources/ImageProcessingJNITest01.dll");
     }
 
-    public native int processImage(byte[] rawData, int width, int height, int bytesPerLine);
+    /**
+     * 
+     * @return
+     */
+    public native int getVersion();
+
+    /**
+     * 
+     * @param param
+     * @return
+     */
+    public native byte[] request(byte[] param);
+
+    /**
+     * 
+     * @param callID
+     * @param result
+     */
+    public native void answer(byte[] callID, byte[] result);
+
+    /**
+     * 
+     * @param result
+     */
+    public native void notify(byte[] result);
 
 }
